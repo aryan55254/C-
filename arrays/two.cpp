b111f70e8f31a3e1d539a2fe99813855cd8c4e51
aryan55254 <aryanmishra55254@gmail.com>
2025-01-07 19:27:29 +0530

    stuff

---
+++ arrays/two.cpp
@@ -0,0 +1,9 @@
+#include "common_include.h"
+using namespace std;
+int main(){
+int numbers[5] = {10, 20, 30, 40, 50};
+for (int i = 0; i < 5; i++) {
+    cout << "numbers[" << i << "] = " << numbers[i] << endl;
+}
+
+}
